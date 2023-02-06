cc1be9b83811250087caa310a9e969addf9f29f6
Emmanuel <emmanuelugwueze6@gmail.com>
2023-02-07 00:44:14 +0100

    init quiz controller and services

---
+++ controller/quiz.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+
+
+class QuizController{
+
+
+    async postMaths(req:Request, res:Response) {
+        
+    }
+}
+
+export default new QuizController();
