a633796a7ed09b87f035277620c7853b35bef57d
Emmanuel <emmanuelugwueze6@gmail.com>
2023-02-08 02:37:47 +0100

    adding pro maths questions

--- controller/maths.controller.js
+++ controller/maths.controller.js
@@ -17,7 +17,7 @@ class QuizController {
     postMaths(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             console.log(req.body.answers);
-            const postReq = yield quiz_services_js_1.default.postQuiz({ question: req.body.question, answers: { "a": req.body.answers[0], "b": req.body.answers[1], }, correct_answer: req.body.answers[1], level: 'beginner' });
+            const postReq = yield quiz_services_js_1.default.postQuiz({ question: req.body.question, answers: { "a": req.body.answers[0], "b": req.body.answers[1], }, correct_answer: req.body.answers[1], level: 'professional' });
             return res.status(201).send({ status: true, message: "question addded succcessfully" });
         });
     }
